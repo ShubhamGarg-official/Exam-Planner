a51f17555d3ca8f82c98b2c20461fcf7b830886d
Shubham Garg <shubh221106@gmail.com>
2025-07-12 12:07:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 from datetime import datetime, timedelta
+from fpdf import FPDF
 
 # ------------------ Chapter Data ------------------
 data = {
